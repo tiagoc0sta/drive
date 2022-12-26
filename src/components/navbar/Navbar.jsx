04160be59c7bbe452deb58a2414849cb6c67875b
Tiago Costa <tiagodpv@gmail.com>
2022-12-25 23:17:20 -0500

    add global styles

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styles from "./Navbar.module.css";
+import {
+  AiOutlineMenu,
+  AiOutlineClose,
+  AiOutlineSearch,
+  AiOutlineUser,
+} from "react-icons/ai";
+import Logo from "../../images/logo.png";
+
+const Navbar = () => {
+  return (
+    <header className={styles.navbar}>
+      <img src={Logo} alt='/' />;
+      <nav>
+        <ul className={styles.menu}>
+          <li>
+            <a href=''>Leran More</a>
+          </li>
+          <li>
+            <a href=''>Log in</a>
+          </li>
+          <li>
+            <a href=''>Sign up</a>
+          </li>
+          <li>
+            <AiOutlineSearch />
+          </li>
+          <li>
+            <AiOutlineUser />
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
