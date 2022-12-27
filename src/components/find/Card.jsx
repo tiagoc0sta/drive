ae8a27f20f00c2d8aa00afe6a7df3d4d8399dfd6
Tiago Costa <tiagodpv@gmail.com>
2022-12-27 11:21:47 -0500

    last 2.0

--- src/components/find/Card.jsx
+++ src/components/find/Card.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import styles from './Find.module.css';
 
-import { Navigation, Pagination, Scrollbar, Ally } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
-
 import 'swiper/css';
 import 'swiper/css/navigation';
 
