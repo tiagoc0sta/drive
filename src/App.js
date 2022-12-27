662aab41dbab551cad124cd00738e3e0912138c1
Tiago Costa <tiagodpv@gmail.com>
2022-12-27 11:21:01 -0500

    final version

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Driver from './components/driver/Driver';
 import Find from './components/find/Find';
+import Footer from './components/footer/Footer';
 import Hero from './components/hero/Hero';
 import Luxury from './components/luxury/Luxury';
 import Navbar from './components/navbar/Navbar';
@@ -13,6 +14,7 @@ function App() {
       <Find />
       <Driver />
       <Luxury />
+      <Footer />
     </div>
   );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import styles from './Footer.module.css';
+import Logo from '../../images/logo_dark.png';
 
 const Footer = () => {
   return (
-    <div>
-      <div></div>
+    <div className={styles.footer}>
+      <div className={styles.container}>
+        <img src={Logo} alt='/' />
+        <button>Share A Car</button>
+      </div>
     </div>
   );
 };
